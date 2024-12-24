0ea08f575a44927d5e6c43e4cfed453365dc5507
alekslynx <siriusmc@mail.ru>
2024-12-24 19:34:38 +0100

    v 1.3.4

--- orders_service/main.py
+++ orders_service/main.py
@@ -6,7 +6,7 @@ from strawberry.fastapi import GraphQLRouter
 from app import database, logger
 from app.kafka import start_consumer
 from app.graphql import schema
-from app.database import init_db
+from app.database import init_db, SessionLocal
 
 app = FastAPI(
     title="Order Management Microservice API",
@@ -36,7 +36,13 @@ def startup():
 
 # Функция для получения контекста
 async def get_context(request: Request):
-    return {"request": request}
+    # Создаем объект сессии
+    db_session = SessionLocal()
+    # Возвращаем её вместе с request
+    return {
+        "request": request,
+        "db": db_session
+    }
 
 # Создаем GraphQL-приложение
 graphql_app = GraphQLRouter(schema, context_getter=get_context)
